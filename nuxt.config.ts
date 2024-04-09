520472ba660d89f8dca23b874586837b7a33f9ee
Ryan Clements <ryanclementshax@gmail.com>
2024-04-09 10:27:02 -0400

    add nuxt-security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["nuxt-security"],
+  security: {
+    headers: {
+      // To allow for nuxt dev tools in development
+      crossOriginEmbedderPolicy:
+        process.env.NODE_ENV === "development" ? "unsafe-none" : "require-corp",
+    },
+  },
+});
