3f1eb572137b10d829ed67cb24f248317f95b04a
Ryan Clements <ryanclementshax@gmail.com>
2024-04-18 21:03:20 -0400

    remove security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,14 +3,17 @@ import type { ManifestV2 } from "@uniformdev/context";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["nuxt-security", "@uniformdev/uniform-nuxt"],
-  security: {
-    headers: {
-      // To allow for nuxt dev tools in development
-      crossOriginEmbedderPolicy:
-        process.env.NODE_ENV === "development" ? "unsafe-none" : "require-corp",
-    },
-  },
+  modules: [
+    // "nuxt-security",
+    "@uniformdev/uniform-nuxt",
+  ],
+  // security: {
+  //   headers: {
+  //     // To allow for nuxt dev tools in development
+  //     crossOriginEmbedderPolicy:
+  //       process.env.NODE_ENV === "development" ? "unsafe-none" : "require-corp",
+  //   },
+  // },
   uniform: {
     projectId: process.env.UNIFORM_PROJECT_ID,
     readOnlyApiKey: process.env.UNIFORM_API_KEY,
