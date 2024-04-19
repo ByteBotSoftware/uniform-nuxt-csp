fd6863196d8b1c5b52a1f6fb16b64a7958703a9f
Ryan Clements <ryanclementshax@gmail.com>
2024-04-18 20:36:59 -0400

    create basic uniform app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,21 @@
+import type { ManifestV2 } from "@uniformdev/context";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["nuxt-security"],
+  modules: ["nuxt-security", "@uniformdev/uniform-nuxt"],
   security: {
     headers: {
       // To allow for nuxt dev tools in development
       crossOriginEmbedderPolicy:
         process.env.NODE_ENV === "development" ? "unsafe-none" : "require-corp",
     },
+  },
+  uniform: {
+    projectId: process.env.UNIFORM_PROJECT_ID,
+    readOnlyApiKey: process.env.UNIFORM_API_KEY,
+    apiHost: process.env.UNIFORM_CLI_BASE_URL,
+    manifest: {} as ManifestV2,
+    defaultConsent: true,
   },
 });
