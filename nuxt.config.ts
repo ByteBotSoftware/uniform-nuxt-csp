0c603aab89d4ec4168da55ece3a2b235dcfae1d8
Ryan Clements <ryanclementshax@gmail.com>
2024-04-25 14:51:05 -0400

    get basic nuxt-security policy working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import type { ManifestV2 } from "@uniformdev/context";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["~/styles/global.css"],
-  modules: ["@uniformdev/uniform-nuxt"],
+  modules: ["@uniformdev/uniform-nuxt", "nuxt-security"],
   uniform: {
     projectId: process.env.UNIFORM_PROJECT_ID,
     readOnlyApiKey: process.env.UNIFORM_API_KEY,
@@ -13,5 +13,29 @@ export default defineNuxtConfig({
     manifest: manifest as ManifestV2,
     defaultConsent: true,
     outputType: "standard",
+  },
+  security: {
+    headers: {
+      // To allow for nuxt dev tools in development
+      crossOriginEmbedderPolicy:
+        process.env.NODE_ENV === "development" ? "unsafe-none" : "require-corp",
+      // xXSSProtection: '1; mode=block',
+      crossOriginResourcePolicy: "cross-origin",
+      contentSecurityPolicy: {
+        // "base-uri": ["'self'"],
+        // "font-src": ["'self'", "https:", "data:"],
+        // "form-action": ["'self'"],
+        "frame-ancestors": [
+          "'self'",
+          "https://uniform.app",
+          // "https://app.contentstack.com",
+        ],
+        // "img-src": ["'self'", "https:", "data:"],
+        // "object-src": ["'none'"],
+        // "script-src-attr": ["'none'"],
+        // "style-src": ["'self'", "https:", "'unsafe-inline'"],
+        // "upgrade-insecure-requests": true,
+      },
+    },
   },
 });
