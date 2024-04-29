21268c1ead1df597dedce1cdfdf27b08fde580bc
Ryan Clements <ryanclementshax@gmail.com>
2024-04-29 13:30:00 -0400

    default to / for slug

--- pages/uniform-preview.vue
+++ pages/uniform-preview.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 definePageMeta({
   redirect: (a) => {
-    const { slug, secret, id, is_incontext_editing_mode } = a.query
-    return `${slug}?preview=1&secret=${secret}&id=${id}&is_incontext_editing_mode=${is_incontext_editing_mode}`
+    const { slug, secret, id, is_incontext_editing_mode } = a.query;
+    return `${
+      slug ?? "/"
+    }?preview=1&secret=${secret}&id=${id}&is_incontext_editing_mode=${is_incontext_editing_mode}`;
   },
-})
+});
 </script>
