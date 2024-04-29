e4317f19f163ed4e89c58505f2aa736c985357e5
Ryan Clements <ryanclementshax@gmail.com>
2024-04-29 13:48:25 -0400

    discard slug

--- pages/uniform-preview.vue
+++ pages/uniform-preview.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 definePageMeta({
   redirect: (to) => {
-    const { path, ...rest } = to.query;
+    const { path, slug: _, ...rest } = to.query;
     return {
       path: (Array.isArray(path) ? path[0] : path) ?? "/",
       query: rest,
