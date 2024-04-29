f4afe360c05d1ef0d3c64b7587c624297efc45d9
Ryan Clements <ryanclementshax@gmail.com>
2024-04-29 13:21:17 -0400

    use page redirect workaround

---
+++ pages/uniform-preview.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+definePageMeta({
+  redirect: (a) => {
+    const { slug, secret, id, is_incontext_editing_mode } = a.query
+    return `${slug}?preview=1&secret=${secret}&id=${id}&is_incontext_editing_mode=${is_incontext_editing_mode}`
+  },
+})
+</script>
